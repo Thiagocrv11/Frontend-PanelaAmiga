bd1bb1ef938838a314dcb001d689d9c7235a2068
HidekiLeonardo <leonardohideki778@gmail.com>
2023-11-28 20:02:14 -0300

    update

--- script2.js
+++ script2.js
@@ -4,19 +4,13 @@ function mostrarLogin() {
     modal.style.display = 'block';
     overlay.style.display = 'block';
     }
-
-// Função para fechar o modal de login
 function fecharLogin() {
     var modal = document.getElementById('loginModal');
     var overlay = document.getElementById('overlay');
     modal.style.display = 'none';
     overlay.style.display = 'none';
 }
-
-// Função para realizar o login (simulada, substitua com sua lógica real)
 function realizarLogin() {
-    // Implemente a lógica real de autenticação aqui
-    // Este é um exemplo simples onde apenas fecha o modal
     fecharLogin();
     }
 
